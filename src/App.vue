8e3f150875752a13543d19221f1f5c0f69a6f655
wuxianqiang <2631640352@qq.com>
2020-12-13 22:36:20 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld @check="handlecheck" msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  setup (props) {
+    const handlecheck = () => {
+      console.log('todo')
+    }
+    return {
+      HelloWorld,
+      handlecheck
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div @click="handleClick">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      obj: { a: '1' },
+      arr: [1,2,3]
+    }
+  },
+  created () {
+    console.log(this.obj)
+    this.$watch('obj', (newVal) => {
+      console.log(newVal)
+    }),
+    this.$watch('arr', (newVal) => {
+      console.log(newVal)
+    })
+  },
+  methods: {
+    handleClick () {
+      console.log('点击')
+      this.obj.a = Math.random()
+      this.arr.push(Math.random())
+    }
+  },
+  setup () {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
